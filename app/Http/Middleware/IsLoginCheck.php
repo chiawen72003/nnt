4bd5f08adf2a75a269aee9dfca38300fe5b60364
cwc <cwc>
2017-01-04 21:33:37 +0800

    中介層增加：檢查是否已經登入，如果已經登入就直接導到測驗列表

--- app/Http/Middleware/IsLoginCheck.php
+++ app/Http/Middleware/IsLoginCheck.php
@@ -3,9 +3,8 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\Auth;
 
-class RedirectIfAuthenticated
+class IsLoginCheck
 {
     /**
      * Handle an incoming request.
@@ -17,8 +16,8 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/');
+        if ($request->session()->has('user_data')) {
+            return redirect()->route('mem.exam');
         }
 
         return $next($request);
