7372eefdf74f26adc06c67a811b0054edb18f2d8
cwc <cwc>
2017-01-04 20:00:05 +0800

    增加一個登入資料檢查的fun

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -8,7 +8,7 @@ use \Validator;
 use \Session;
 use \DB;
 use \Response;
-
+use App\Http\Providers\MemberClass;
 
 class MemberController extends Controller
 {
@@ -20,6 +20,31 @@ class MemberController extends Controller
 	public function LoginPage(){
 		$data = array();
 		
-		return view('member.login',$data);
+		return view('student.member.login',$data);
 	}
+	
+	public function LoginChk(){
+		$data = array();
+		$fp = Input::all();
+		$check_data = MemberClass::chk_login_data($fp);
+		if($check_data['check_result']){
+			$user_data = $check_data['user_data'];
+			if($user_data['level'] == '91'){
+				return view('student.member.login',$data);
+			}
+			if($user_data['level'] == '9'){
+				session(['user_data' => $user_data]);
+				return redirect()->route('mem.exam');
+			}
+		}
+		
+		return redirect()->back()->with('error', ['your message,here']); 
+	}
+
+	public function LogOut(){
+		app('request')->session()->flush();
+		
+		return redirect()->route('mem.loginpg');
+	}
+	
 }
