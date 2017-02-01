d489b8c6315c6bb8e07b1a654b68c0943de7de72
cwc <cwc>
2017-02-01 10:23:29 +0800

    增加一個檢查管理員是否已經登入的中介層

---
+++ app/Http/Middleware/IsAdLoginCheck.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class IsAdLoginCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if ($request->session()->has('admin_data')) {
+            return redirect()->route('ad.index');
+        }
+
+        return $next($request);
+    }
+}
