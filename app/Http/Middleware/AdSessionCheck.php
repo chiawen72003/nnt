1a659f0b1abc0c16b741acf4cd01f797a37e6ca8
cwc <cwc>
2017-02-01 10:24:41 +0800

    新增一個檢查是否為管理員身份的中介層

---
+++ app/Http/Middleware/AdSessionCheck.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AdSessionCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if (!$request->session()->has('admin_data')) {
+            return redirect()->route('ad.logout');
+        }
+
+        return $next($request);
+    }
+}
