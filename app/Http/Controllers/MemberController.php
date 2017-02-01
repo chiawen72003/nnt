f053b35867ab50e27248049951752b9e274f5edf
cwc <cwc>
2017-02-01 20:54:57 +0800

    學生登入調整、增加管理員登入項目

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Models\MemberAdmin;
 use Illuminate\Http\Request;
 use \Input;
 use \Validator;
@@ -25,17 +26,13 @@ class MemberController extends Controller
 	
 	public function LoginChk(){
 		$data = array();
-		$fp = Input::all();
-		$check_data = MemberClass::chk_login_data($fp);
+        MemberClass::_init();
+		$check_data = MemberClass::chk_login_data();
 		if($check_data['check_result']){
-			$user_data = $check_data['user_data'];
-			if($user_data['level'] == '91'){
-				return view('student.member.login',$data);
-			}
-			if($user_data['level'] == '9'){
-				session(['user_data' => $user_data]);
-				return redirect()->route('mem.exam');
-			}
+            $user_data = $check_data['user_data'];
+            session(['user_data' => $user_data]);
+
+            return redirect()->route('mem.exam');
 		}
 		
 		return redirect()->back()->with('error', ['your message,here']); 
@@ -46,5 +43,100 @@ class MemberController extends Controller
 		
 		return redirect()->route('mem.loginpg');
 	}
+
+    /**
+     * 管理員登入頁面
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function AdLoginPage(){
+
+        $data = array();
+
+        return view('admin.member.login',$data);
+    }
+
+    /**
+     * 管理員登入檢查
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function AdLoginChk(){
+        $data = array();
+        MemberClass::_init();
+        $check_data = MemberClass::chk_ad_login_data();
+        if($check_data['check_result']){
+            $user_data = $check_data['user_data'];
+            session(['admin_data' => $user_data]);
+
+            return redirect()->route('ad.index');
+        }
+
+        return redirect()->back()->with('error', ['your message,here']);
+    }
+
+    /**
+     * 管理員登出
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function AdLogOut(){
+        app('request')->session()->flush();
+
+        return redirect()->route('ad.loginpg');
+    }
+
+    /**
+     * 管理員列表
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function adminList()
+    {
+        $data = array();
+        $data['user_data'] = app('request')->session()->get('user_data');
+        $data['list_data'] = MemberAdmin::get_list();
+
+        return view('admin.admin_list', $data);
+    }
+
+    /**
+     * 新增一筆管理員的資料
+     *
+     */
+    public function  adminAdd()
+    {
+        $fp = Input::all();
+        MemberAdmin::_init($fp);
+        MemberAdmin::add();
+
+        return '';
+    }
+
+    /**
+     * 更新一筆管理員的資料
+     *
+     */
+    public function  adminUpdate()
+    {
+        $fp = Input::all();
+        MemberAdmin::_init($fp);
+        MemberAdmin::update_data();
+
+        return '';
+    }
+
+    /**
+     * 移除一個管理員
+     *
+     */
+    public function  adminDelete()
+    {
+        $fp = Input::all();
+        MemberAdmin::_init($fp);
+        MemberAdmin::delete_data();
+
+        return ;
+    }
 	
 }
