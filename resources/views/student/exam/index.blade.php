9b2bda0cd6b8cefa548a9cf11a1127b5910afa05
cwc <cwc>
2017-02-11 08:44:23 +0800

    新增測驗的首頁

---
+++ resources/views/student/exam/index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	[! Html::style('style/reset.css') !]
+	[! Html::style('style/style.css') !]
+</head>
+<body class="is-login">
+	<div id="header">
+		<div class="header-top">
+			<div id="header-logo"></div>
+		</div>
+		<div id="boad-wrap" class="boad-wrap welcome">
+			<div id="boad-nav">
+				<a href="#">系統公告</a>
+				<a href="[! route('mem.exam') !]">學習</a>
+				<a href="#">成果查詢</a>
+				<a href="[! route('mem.logout') !]">登出</a>
+			</div>
+			<div class="boad-welcome-wrap">
+				<p>歡迎光臨！</p>
+				<p>您是<span class="txt-yellow">[! $user_data['user_name'] !] </span>，身份：學生</p>
+				<p>台中市臺中教育大學[! $user_data['school_grade'] !]年[! $user_data['school_class'] !]班</p>
+			</div>
+			<div class="img-chalk"></div>
+		</div>
+	</div>
+</body>
+</html>
