a92d4cecc74811bb1ec898de59ee25bcc800fc99
cwc <cwc>
2017-02-27 15:24:43 +0800

    試題class由靜態改為動態物件

--- app/Http/Models/QuestionsItem.php
+++ app/Http/Models/QuestionsItem.php
@@ -4,15 +4,27 @@ namespace App\Http\Models;
 class QuestionsItem extends BaseModel {
     protected $table = 'questions_item';
     protected $primaryKey = 'id';
-    public $timestamps = false;
+    //public $timestamps = false;
 
-    public static $item_data = array();
+    private $item_data = array(
+        'title'=>'',
+        'power_dsc'=>'',
+        'model_item_id'=>'0',
+        'model_item_options'=>array(),
+        'feedback_type'=>'0',
+        'feedback_dsc'=>'',
+        'abbreviation'=>'',
+        'exam_paper_id'=>'0',
+        'avatar_type'=>array(),
+        'avatar_dsc'=>array(),
+        'correct_answer'=>array(),
+        'error_answer'=>array(),
+        'score'=>'0',
+    );
 
-    public static function _init($input_data){
-        foreach($input_data as $item){
-            foreach ($item as $key => $value){
-                self::$item_data[$key] = $value;
-            }
+    public function _init($input_data){
+        foreach($input_data as $key => $value){
+            $this->item_data[$key] = $value;
         }
     }
 
@@ -20,21 +32,20 @@ class QuestionsItem extends BaseModel {
      * 新增一個試題資料
      *
      */
-    public static function add(){
-        dd(self::$item_data);
+    public function add(){
         $add_obj = new QuestionsItem();
-        $add_obj -> title =  self::$item_data['title'];
-        $add_obj -> model_item_id =  self::$item_data['model_item_id'];
-        $add_obj -> model_item_options =   json_encode(self::$item_data['model_item_options']);
-        $add_obj -> feedback_type =  self::$item_data['feedback_type'];
-        $add_obj -> feedback_dsc =  self::$item_data['feedback_dsc'];
-        $add_obj -> abbreviation =  self::$item_data['abbreviation'];
-        $add_obj -> exam_paper_id =  self::$item_data['exam_paper_id'];
-        $add_obj -> avatar_type =  json_encode(self::$item_data['avatar_type']);
-        $add_obj -> avatar_dsc =  json_encode(self::$item_data['avatar_dsc']);
-        $add_obj -> correct_answer =  json_encode(self::$item_data['correct_answer']);
-        $add_obj -> error_answer =  json_encode(self::$item_data['error_answer']);
-        $add_obj -> power_dsc =  self::$item_data['power_dsc'];
+        $add_obj -> title =  $this->item_data['title'];
+        $add_obj -> model_item_id =  $this->item_data['model_item_id'];
+        $add_obj -> model_item_options =   json_encode($this->item_data['model_item_options']);
+        $add_obj -> feedback_type =  $this->item_data['feedback_type'];
+        $add_obj -> feedback_dsc =  $this->item_data['feedback_dsc'];
+        $add_obj -> abbreviation =  $this->item_data['abbreviation'];
+        $add_obj -> exam_paper_id =  $this->item_data['exam_paper_id'];
+        $add_obj -> avatar_type =  json_encode($this->item_data['avatar_type']);
+        $add_obj -> avatar_dsc =  json_encode($this->item_data['avatar_dsc']);
+        $add_obj -> correct_answer =  json_encode($this->item_data['correct_answer']);
+        $add_obj -> error_answer =  json_encode($this->item_data['error_answer']);
+        $add_obj -> power_dsc =  $this->item_data['power_dsc'];
         $add_obj -> save();
 
         return $add_obj->id;
@@ -44,20 +55,20 @@ class QuestionsItem extends BaseModel {
      * 更新一個試題資料
      *
      */
-    public static function update_data(){
-        $add_obj = QuestionsItem::find(self::$item_data['id']);
-        $add_obj -> title =  self::$item_data['title'];
-        $add_obj -> model_item_id =  self::$item_data['model_item_id'];
-        $add_obj -> model_item_options =   json_encode(self::$item_data['model_item_options']);
-        $add_obj -> feedback_type =  self::$item_data['feedback_type'];
-        $add_obj -> feedback_dsc =  self::$item_data['feedback_dsc'];
-        $add_obj -> abbreviation =  self::$item_data['abbreviation'];
-        $add_obj -> exam_paper_id =  self::$item_data['exam_paper_id'];
-        $add_obj -> avatar_type =  json_encode(self::$item_data['avatar_type']);
-        $add_obj -> avatar_dsc =  json_encode(self::$item_data['avatar_dsc']);
-        $add_obj -> correct_answer =  json_encode(self::$item_data['correct_answer']);
-        $add_obj -> error_answer =  json_encode(self::$item_data['error_answer']);
-        $add_obj -> power_dsc =  self::$item_data['power_dsc'];
+    public function update_data(){
+        $add_obj = QuestionsItem::find($this->item_data['id']);
+        $add_obj -> title =  $this->item_data['title'];
+        $add_obj -> model_item_id =  $this->item_data['model_item_id'];
+        $add_obj -> model_item_options =   json_encode($this->item_data['model_item_options']);
+        $add_obj -> feedback_type =  $this->item_data['feedback_type'];
+        $add_obj -> feedback_dsc =  $this->item_data['feedback_dsc'];
+        $add_obj -> abbreviation =  $this->item_data['abbreviation'];
+        $add_obj -> exam_paper_id =  $this->item_data['exam_paper_id'];
+        $add_obj -> avatar_type =  json_encode($this->item_data['avatar_type']);
+        $add_obj -> avatar_dsc =  json_encode($this->item_data['avatar_dsc']);
+        $add_obj -> correct_answer =  json_encode($this->item_data['correct_answer']);
+        $add_obj -> error_answer =  json_encode($this->item_data['error_answer']);
+        $add_obj -> power_dsc =  $this->item_data['power_dsc'];
         $add_obj -> save();
 
         return $add_obj->id;
@@ -67,22 +78,22 @@ class QuestionsItem extends BaseModel {
      * 取出下一個試題資料
      *
      */
-    public static function next_data($msg)
+    public function next_data($msg)
     {
         $get_data = false;
         $get_index = 0;
-        $item_obj = QuestionsItem::where('exam_paper_id',self::$item_data['exam_paper_id'])
+        $item_obj = QuestionsItem::where('exam_paper_id',$this->item_data['exam_paper_id'])
                     ->orderBy('id','ASC');
         $item_obj =  $item_obj->get();
         if(count($item_obj) > 0){
             //一開始載入的時候是0
-            if( self::$item_data['id'] == 0){
+            if( $this->item_data['id'] == 0){
                 $msg['type'] = 'success';
                 $msg['has_next'] = true;
                 $msg['data'] = $item_obj[0]->toArray();
             }else{
                 foreach($item_obj as $key => $temp_obj){
-                    if($temp_obj['id'] > self::$item_data['id'] AND !$get_data){
+                    if($temp_obj['id'] > $this->item_data['id'] AND !$get_data){
                         $msg['type'] = 'success';
                         $msg['data'] = $temp_obj->toArray();
                         $get_index = $key;
@@ -111,16 +122,16 @@ class QuestionsItem extends BaseModel {
     * 取出上一個試題資料
     *
     */
-    public static function back_data($msg)
+    public function back_data($msg)
     {
         $get_data = false;
         $get_index = 0;
-        $item_obj = QuestionsItem::where('exam_paper_id',self::$item_data['exam_paper_id'])
+        $item_obj = QuestionsItem::where('exam_paper_id',$this->item_data['exam_paper_id'])
             ->orderBy('id','DESC');
         $item_obj =  $item_obj->get();
         if(count($item_obj) > 0){
             //如果是新增試題退回上一題
-            if( self::$item_data['id'] == 0 ){
+            if( $this->item_data['id'] == 0 ){
                 $msg['type'] = 'success';
                 $msg['has_back'] = true;
                 $msg['data'] = $item_obj[0]->toArray();
@@ -128,7 +139,7 @@ class QuestionsItem extends BaseModel {
                 //一般試題編輯的話
                 foreach($item_obj as $key => $temp_obj)
                 {
-                    if(!$get_data AND $temp_obj['id'] <  self::$item_data['id'])
+                    if(!$get_data AND $temp_obj['id'] <  $this->item_data['id'])
                     {
                         $msg['type'] = 'success';
                         $msg['data'] = $temp_obj->toArray();
@@ -156,17 +167,17 @@ class QuestionsItem extends BaseModel {
     * 刪除一個試題資料
     *
     */
-    public static function delete_data($msg)
+    public function delete_data($msg)
     {
         $get_data = false;
         $get_index = 0;
-        $item_obj = QuestionsItem::where('exam_paper_id',self::$item_data['exam_paper_id'])
+        $item_obj = QuestionsItem::where('exam_paper_id',$this->item_data['exam_paper_id'])
             ->orderBy('id','DESC');
         $item_obj =  $item_obj->get();
         if(count($item_obj) > 0){
             foreach($item_obj as $key => $temp_obj)
             {
-                if( $temp_obj['id'] ==  self::$item_data['id'])
+                if( $temp_obj['id'] ==  $this->item_data['id'])
                 {
                     $msg['type'] = 'success';
                     $get_index = $key;
@@ -184,7 +195,7 @@ class QuestionsItem extends BaseModel {
             }
 
             //刪除實際資料
-            $item = QuestionsItem::find(self::$item_data['id']);
+            $item = QuestionsItem::find($this->item_data['id']);
             $item->delete();
         }
 
@@ -195,7 +206,7 @@ class QuestionsItem extends BaseModel {
      * 取得試卷內試題的數量
      *
      */
-    public static function get_paper_item_num($paper_id)
+    public function get_paper_item_num($paper_id)
     {
         $return_data = array();
 
