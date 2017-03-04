3b86c04cb2b706cd25276d81dba66e61d02baa58
cwc <cwc>
2017-03-04 11:52:19 +0800

    單元編輯頁面改版

--- resources/views/admin/unitedit.blade.php
+++ resources/views/admin/unitedit.blade.php
@@ -1,134 +1,119 @@
-<span>[! $title !]單元</span>
-<table>
-    <tr>
-        <td>版本</td>
-        <td>
-            <select name="publisher_id" id="publisher_id">
-                <option value="">==請選擇==</option>
-                <option value="19">單代理人</option>
-                <option value="20">雙代理人</option>
-                <option value="21">多代理人</option>
-            </select>
-        </td>
-    </tr>
-    <tr>
-        <td>領域</td>
-        <td>
-            <select name="subject_id" id="subject_id">
-                <option value="">==請選擇==</option>
-                @foreach($subject_list as $key => $value)
-                    <option value="[! $key !]">[! $value !]</option>
-                @endforeach
-            </select>
-        </td>
-    </tr>
-    <tr>
-        <td>冊</td>
-        <td>
-            <select name="vol" id="vol">
-                <option value="">==請選擇==</option>
-                @for($i=1;$i<19;$i++)
-                    <option value="[! $i !]">[! $i !]</option>
-                @endfor
-            </select>
-        </td>
-    </tr>
-    <tr>
-        <td>適用年級</td>
-        <td>
-            <select name="grade" id="grade">
-                <option value="">==請選擇==</option>
-                @for($i=3;$i<17;$i++)
-                    <option value="[! $i !]">[! $i !]</option>
-                @endfor
-            </select>
-        </td>
-    </tr>
-    <tr>
-        <td>單元</td>
-        <td>
-        <select name="unit" id="unit">
-            <option value="">==請選擇==</option>
-            @for($i=1;$i<100;$i++)
-                <option value="[! $i !]">[! $i !]</option>
-            @endfor
-        </select>
-        </td>
-    </tr>
-    <tr>
-        <td>名稱</td>
-        <td>
-            <input type="text" name="concept" id="concept" value="">
-        </td>
-    </tr>
-    <tr>
-        <td>能力指標</td>
-        <td>
-            <select name="indicator_nums" id="indicator_nums">
-                <option value="">==請選擇==</option>
-                @for($i=1;$i<100;$i++)
-                    <option value="[! $i !]">[! $i !]</option>
-                @endfor
-            </select>
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <span><a href="#" onclick="structure_list()">回上一頁</a></span>
-            <span><a href="#" onclick='[!isset($old_data)?'update("'.$old_data["cs_sn"].'")':'save()'!]'>存檔</a></span>
-        </td>
-        <td></td>
-    </tr>
-</table>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    [! Html::style('style/reset.css') !]
+    [! Html::style('style/style.css') !]
+    <script>
+        window.onload = function (){
+            var docHeight = document.body.offsetHeight,
+                wrapHeight = docHeight - document.getElementById("header").offsetHeight;
+
+            // 設定page-body元素高度
+            document.getElementById("page-body").style.minHeight = wrapHeight + "px";
+        }
+    </script>
+</head>
+<body class="is-login">
+<div id="header">
+    <div class="header-top">
+        <div id="header-logo"></div>
+    </div>
+    <div id="boad-wrap" class="boad-wrap">
+        <div id="boad-nav">
+            <a href="[! route('ad.index') !]" title="單元列表">單元列表</a>
+            <a href="[! route('ad.subject.list') !]" title="科目列表">科目列表</a>
+            <a href="[! route('mem.admin.list') !]" title="管理員列表">管理員列表</a>
+            <a href="[! route('ad.school.list') !]" title="學校列表">學校列表</a>
+            <a href="[! route('ad.logout') !]" title="登出系統">登出</a>
+        </div>
+        <div class="boad-detail-wrap">
+            <p>管理員：<span class="txt-yellow">autotutor</span></p>
+        </div>
+        <div class="img-chalk"></div>
+    </div>
+</div>
+<div id="page-container">
+    <div id="page-body">
+        <h1 class="section-title title-structure">單元結構編制</h1>
+        <div class="section-content">
+            [! Form::open(array('url'=>route($form_path),'id'=>'addForm', 'name'=>'addForm', 'files' => true)) !]
+            [! Form::hidden('domain', 'addForm') !]
+
+            <div class="select-group">
+                    <div class="label-title">代理人類別</div>
+                    <select name="module_type" id="module_type">
+                        <option value="">請選擇代理人類別</option>
+                        <option value="1">單代理人</option>
+                        <option value="2">雙代理人</option>
+                        <option value="3">多代理人</option>
+                    </select>
+                </div>
+                <div class="select-group">
+                    <div class="label-title">領域</div>
+                    <select name="subject" id="subject">
+                        <option value="">請選擇領域</option>
+                        @foreach($subject_list as $key => $value)
+                            <option value="[! $key !]">[! $value !]</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="select-group">
+                    <div class="label-title">冊別</div>
+                    <select name="vol" id="vol">
+                        <option value="">請選擇冊別</option>
+                        @for($i=1;$i<19;$i++)
+                            <option value="[! $i !]">[! $i !]</option>
+                        @endfor
+                    </select>
+                </div>
+                <div class="select-group">
+                    <div class="label-title">適用年級</div>
+                    <select name="grade" id="grade">
+                        <option value="">請選擇適用年級</option>
+                        @for($i=3;$i<17;$i++)
+                            <option value="[! $i !]">[! $i !]</option>
+                        @endfor
+                    </select>
+                </div>
+                <div class="select-group">
+                    <div class="label-title">單元</div>
+                    <select name="unit" id="unit">
+                        <option value="">請選擇單元</option>
+                        @for($i=1;$i<100;$i++)
+                            <option value="[! $i !]">[! $i !]</option>
+                        @endfor
+                    </select>
+                </div>
+                <div class="select-group">
+                    <div class="label-title"><span class="txt-red">*</span>名稱</div>
+                    <input class="select-input" type="text" name="title" id="title" value=""><span class="txt-red">(必填)</span>
+                </div>
+                <div class="select-group">
+                    <div class="label-title">能力指標</div>
+                    <select name="indicator_nums" id="indicator_nums">
+                        <option value="">請選擇能力指標</option>
+                        @for($i=1;$i<100;$i++)
+                            <option value="[! $i !]">[! $i !]</option>
+                        @endfor
+                    </select>
+                </div>
+                <div class="select-group">
+                    <div class="label-title"><span class="txt-red">*</span>示意圖</div>
+                    <input class="select-input" type="file" name="img" id="img" accept="image/*"><span class="txt-red">(必填)</span>
+                </div>
+                <div class="form-button-wrap">
+                    <input class="btn-yellow" type="button" onclick='history.back()' value="回上一頁" />
+                    <input class="btn-yellow" type="submit"  value="輸入完畢，建立結構" />
+                </div>
+            [! Form::close() !]
+        </div>
+    </div>
+</div>
+[! Html::script('js/jquery-1.11.3.js') !]
+
 <script>
-    $( document ).ready(function() {
-       @if(isset($old_data))
-            @foreach($old_data as $key => $value)
-            $('#[! $key !]').val('[!$value!]');
-            @endforeach
-       @endif
-    });
-
-    function save() {
-        $.ajax({
-            url: "[! route('ad.unit.add.data') !]",
-            type: 'POST',
-            data: {
-                _token: token,
-                module_type: $('#module_type').val(),
-                subject: $('#subject').val(),
-                vol: $('#vol').val(),
-                grade: $('#grade').val(),
-                unit: $('#unit').val(),
-                title: $('#title').val(),
-                indicator_nums:  $('#indicator_nums').val(),
-            },
-            success: function (data) {
-                console.log(data);
-                //structure_list();
-            }
-        });
-    }
-
-    function update(getID) {
-        $.ajax({
-            url: "[! route('ad.unit.update.data') !]",
-            type: 'POST',
-            data: {
-                _token: token,
-                cs_sn: getID,
-                module_type: $('#module_type').val(),
-                subject: $('#subject').val(),
-                vol: $('#vol').val(),
-                grade: $('#grade').val(),
-                unit: $('#unit').val(),
-                title: $('#title').val(),
-                indicator_nums:  $('#indicator_nums').val(),
-            },
-            success: function (data) {
-                //console.log(data);
-                structure_list();
-            }
-        });
-    }
 </script>
+</body>
+</html>
