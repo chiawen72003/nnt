166d51b746f94d099b8e2f1ace944f8417b4eaf9
cwc <cwc>
2017-03-04 21:35:13 +0800

    觀看操作紀錄由原本直接顯示，改成先看操作列表

--- resources/views/student/exam/list.blade.php
+++ resources/views/student/exam/list.blade.php
@@ -50,12 +50,12 @@
 						<div class="select-button-wrrap">
 							<a class="btn btn-yellow" onclick='send("[!$value['id']!]")'>學習</a>
 							@if( isset($exam_review_data[$value['id']]) )
-                                <a class="btn btn-gray" href="[! route('mem.exam.viewrecord',[$value['id']]) !]">觀看紀錄</a>
+								<a class="btn btn-gray" href="[! route('mem.exam.viewrecord.list',[$value['id']]) !]">觀看紀錄</a>
 							@else
                                 @if( $value['has_exam_record'] === false  )
-                                    <a class="btn btn-green" onclick="alert('請先完成整個單元測試!!');">觀看紀錄</a>
+									<a class="btn btn-green" onclick="alert('請先完成整個單元測試!!');">觀看紀錄</a>
                                 @else
-                                    <a class="btn btn-green" href="[! route('mem.exam.viewrecord',[$value['id']]) !]">觀看紀錄</a>
+									<a class="btn btn-green" href="[! route('mem.exam.viewrecord.list',[$value['id']]) !]">觀看紀錄</a>
                                 @endif
 							@endif
 						</div>
