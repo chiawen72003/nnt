452965e1aab66311797be6af8b249d121a94def5
cwc <cwc>
2017-03-04 21:33:16 +0800

    增加一個操作紀錄列表的route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,6 +24,7 @@ Route::post('/Mem/LoginChk', ['middleware' => 'LoginDataCheck','as' => 'mem.logi
 //學生端 測驗
 Route::get('/Mem', ['middleware' => 'MemSessionCheck','as' => 'mem.index', 'uses' => 'ExamController@index']);
 Route::get('/Mem/Exam', ['middleware' => 'MemSessionCheck','as' => 'mem.exam', 'uses' => 'ExamController@examList']);
+Route::get('/Mem/Exam/View/Record/list/{id}', ['middleware' => 'MemSessionCheck','as' => 'mem.exam.viewrecord.list', 'uses' => 'ExamController@viewExamRecordList']);
 Route::post('/Mem/Exam/TestPG', ['middleware' => 'MemSessionCheck','as' => 'mem.exam.testpg', 'uses' => 'ExamController@testPage']);
 Route::post('/Mem/Exam/GetModelPage', ['middleware' => 'MemSessionCheck','as' => 'mem.exam.getmodelpage', 'uses' => 'ExamController@GetModelPage']);
 Route::post('/Mem/Exam/UpdateRecord', ['middleware' => 'MemSessionCheck','as' => 'mem.exam.updateRecord', 'uses' => 'ExamController@setExamRecord']);
