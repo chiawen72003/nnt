bfb9c08b5da7e6355d8d4f8868734fa54bf8d42e
cwc <cwc>
2017-03-07 21:48:46 +0800

    增加一個控制能否更新操作紀錄的參數值 can_update_record。答案分析時增加一個變數用來判斷是否已經有分析結果了

--- resources/views/student/exam/modules/computation_a_AT_no_answer.blade.php
+++ resources/views/student/exam/modules/computation_a_AT_no_answer.blade.php
@@ -34,6 +34,7 @@
 	function doNext() {
         if (right_ans[0].jump != null)
 		{
+            can_update_record = true;
             student_ans = '';
             if (right_ans[0].jump == '999') {
                 now_paper_index++;
