433cd3169342ef955cccc7a1003d2ccffc73d95d
cwc <cwc>
2017-04-05 21:29:07 +0800

    增加 系統公告列表的頁面

---
+++ resources/views/admin/news_list.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    [! Html::style('style/reset.css') !]
+    [! Html::style('style/style.css') !]
+    <script>
+        window.onload = function (){
+            var docHeight = document.body.offsetHeight,
+                wrapHeight = docHeight - document.getElementById("header").offsetHeight;
+
+            // 設定page-body元素高度
+            document.getElementById("page-body").style.minHeight = wrapHeight + "px";
+        }
+    </script>
+</head>
+<body class="is-login">
+<div id="header">
+    <div class="header-top">
+        <div id="header-logo"></div>
+    </div>
+    <div id="boad-wrap" class="boad-wrap">
+        <div id="boad-nav">
+            <a href="[! route('ad.news.list') !]" title="建立系統公告">建立系統公告</a>
+            <a href="[! route('ad.index') !]" title="單元列表">單元列表</a>
+            <a href="[! route('ad.subject.list') !]" title="科目列表">科目列表</a>
+            <a href="[! route('mem.admin.list') !]" title="管理員列表">管理員列表</a>
+            <a href="[! route('ad.school.list') !]" title="學校列表">學校列表</a>
+            <a href="[! route('ad.logout') !]" title="登出系統">登出</a>
+        </div>
+        <div class="boad-detail-wrap">
+            <p>管理員：<span class="txt-yellow">autotutor</span></p>
+        </div>
+        <div class="img-chalk"></div>
+    </div>
+</div>
+<div id="page-container">
+        <div id="page-body">
+            <h1 class="section-title title-buildnews">系統公告管理</h1>
+            <div class="news-wrap">
+                <form id="form-addnews">
+                    <table class="table-manager">
+                        <tr>
+                            <th class="th-number">編號</th>
+                            <th class="th-time">時間</th>
+                            <th class="th-title">公告標題</th>
+                            <th class="th-file">附件檔</th>
+                            <th class="th-button">功能</th>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>2017/02/22</td>
+                            <td>系統操作說明</td>
+                            <td>系統操作說明書.pdf</td>
+                            <td>
+                                <a class="icon-action icon-edit" href="admin_conceptStructure_edit.html"></a>
+                                <a class="icon-action icon-delete" href="#"></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>2017/02/22</td>
+                            <td>系統更新完畢，開放登入</td>
+                            <td></td>
+                            <td>
+                                <a class="icon-action icon-edit" href="admin_conceptStructure_edit.html"></a>
+                                <a class="icon-action icon-delete" href="#"></a>
+                            </td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
+        </div>
+    </div>
+[! Html::script('js/jquery-1.11.3.js') !]
+<script>
+    //確認是否刪除單元
+    function del_unit(get_id,unit_dsc){
+        if(confirm("確定是否刪除下列單元的資料內容嗎?\r\n"+unit_dsc)){
+            $.ajax({
+                url: "[! route('ad.unit.delete') !]",
+                type:'POST',
+                data: {
+                    _token: '[! csrf_token() !]',
+                    getID:get_id,
+                },
+                error: function(xhr) {
+                    //alert('Ajax request 發生錯誤');
+                },
+                success: function(response) {
+                    alert('資料刪除成功!!');
+                    location.reload();
+                }
+            });
+        }
+    }
+</script>
+</body>
+</html>
