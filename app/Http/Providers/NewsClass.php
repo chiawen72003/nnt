2034e2039cdb27998f4590d0c8c9eec2e5d6913b
cwc <cwc>
2017-04-06 20:59:57 +0800

    新增 刪除系統資料

--- app/Http/Providers/NewsClass.php
+++ app/Http/Providers/NewsClass.php
@@ -3,12 +3,19 @@
 namespace App\Http\Providers;
 
 use App\Http\Models\NewsList;
+use Illuminate\Support\Facades\Storage;
 
 class NewsClass
 {
-    public function __construct()
-    {
+    private $input_array = array(
+        'id' => null
+    );
 
+    public function __construct($input_data = array())
+    {
+        foreach($input_data as $k => $v){
+            $this->input_array[$k] = $v;
+        }
     }
 
     /**
@@ -29,4 +36,24 @@ class NewsClass
     }
 
 
+    /**
+     * 移除一筆系統資料
+     *
+     */
+    public function delete_data()
+    {
+        if($this->input_array['id'])
+        {
+            $temp_data = NewsList::where('id',$this->input_array['id'])->get();
+            foreach ($temp_data as $v){
+                if($v['file_path'] > '' AND file_exists('upfire/file'.$v['file_path']))
+                {
+                    unlink('upfire/file'.$v['file_path']);
+                }
+            }
+            NewsList::destroy($this->input_array['id']);
+        }
+
+        return ;
+    }
 }
