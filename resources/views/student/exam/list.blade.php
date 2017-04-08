a36039ab13c8223f50efc77de2b95886704424a1
cwc <cwc>
2017-04-08 11:26:01 +0800

    套上layout版面

--- resources/views/student/exam/list.blade.php
+++ resources/views/student/exam/list.blade.php
@@ -1,38 +1,6 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-	[! Html::style('style/reset.css') !]
-	[! Html::style('style/style.css') !]
-	<script>
-	window.onload = function (){
-		var docHeight = document.body.offsetHeight,
-			wrapHeight = docHeight - document.getElementById("header").offsetHeight;
-
-		// 設定page-body元素高度
-		document.getElementById("page-body").style.minHeight = wrapHeight + "px";
-	}
-	</script>
-</head>
-<body class="is-login">
-<div id="header">
-	<div class="header-top">
-		<div id="header-logo"></div>
-	</div>
-	<div id="boad-wrap" class="boad-wrap">
-		<div id="boad-nav">
-			<a href="#">系統公告</a>
-			<a href="[! route('mem.exam') !]">學習</a>
-			<a href="[! route('mem.achievement') !]">成果查詢</a>
-			<a href="[! route('mem.logout') !]">登出</a>
-		</div>
-		<div class="boad-detail-wrap">
-			<p>台中市臺中教育大學[! $user_data['school_grade'] !]年[! $user_data['school_class'] !]班<span class="txt-yellow">[! $user_data['user_name'] !]</span></p>
-		</div>
-		<div class="img-chalk"></div>
-	</div>
-</div>
+@extends('student.layout.layout')
+@section('content')
 <div id="page-container">
 	<div id="page-body">
 		<h1 class="section-title title-use">使用說明<span>請直接點擊要學習的單元圖片</span></h1>
@@ -80,5 +48,4 @@
         $('#beginTest').submit();
     }
 </script>
-</body>
-</html>
+@stop
