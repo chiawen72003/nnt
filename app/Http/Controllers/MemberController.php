eeae1802e2e8e49c360d5e0d52e392572dbea624
cwc <cwc>
2017-04-10 21:48:29 +0800

    靜態方法都改為動態物件呼叫

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Models\MemberAdmin;
 use Illuminate\Http\Request;
 use \Input;
 use \Validator;
@@ -10,50 +9,54 @@ use \Session;
 use \DB;
 use \Response;
 use App\Http\Providers\MemberClass;
+use App\Http\Providers\MemberAdminClass;
 
 class MemberController extends Controller
 {
-	public function __construct()
-	{
-		// $this->middleware('guest');
-	}
-	
-	public function LoginPage(){
-		$data = array();
-		
-		return view('student.member.login',$data);
-	}
-	
-	public function LoginChk(){
-		$data = array();
-        MemberClass::_init();
-		$check_data = MemberClass::chk_login_data();
-		if($check_data['check_result']){
+    public function __construct()
+    {
+        // $this->middleware('guest');
+    }
+
+    public function LoginPage()
+    {
+        $data = array();
+
+        return view('student.member.login', $data);
+    }
+
+    public function LoginChk()
+    {
+        $member_class_obj = new MemberClass();
+        $check_data = $member_class_obj -> chk_login_data();
+        if ($check_data['check_result']) {
             $user_data = $check_data['user_data'];
             session(['user_data' => $user_data]);
 
             return redirect()->route('mem.index');
-		}
-		
-		return redirect()->back()->with('error', ['your message,here']); 
-	}
+        }
 
-	public function LogOut(){
-		app('request')->session()->flush();
-		
-		return redirect()->route('mem.loginpg');
-	}
+        return redirect()->back()->with('error', ['your message,here']);
+    }
+
+    public function LogOut()
+    {
+        app('request')->session()->flush();
+
+        return redirect()->route('mem.loginpg');
+    }
 
     /**
      * 管理員登入頁面
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function AdLoginPage(){
+    public function AdLoginPage()
+    {
 
         $data = array();
 
-        return view('admin.member.login',$data);
+        return view('admin.member.login', $data);
     }
 
     /**
@@ -61,11 +64,11 @@ class MemberController extends Controller
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function AdLoginChk(){
-        $data = array();
-        MemberClass::_init();
-        $check_data = MemberClass::chk_ad_login_data();
-        if($check_data['check_result']){
+    public function AdLoginChk()
+    {
+        $member_class_obj = new MemberClass();
+        $check_data = $member_class_obj -> chk_ad_login_data();
+        if ($check_data['check_result']) {
             $user_data = $check_data['user_data'];
             session(['admin_data' => $user_data]);
 
@@ -80,7 +83,8 @@ class MemberController extends Controller
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function AdLogOut(){
+    public function AdLogOut()
+    {
         app('request')->session()->flush();
 
         return redirect()->route('ad.loginpg');
@@ -93,9 +97,10 @@ class MemberController extends Controller
      */
     public function adminList()
     {
+        $member_admin_obj = new MemberAdminClass();
         $data = array();
         $data['user_data'] = app('request')->session()->get('user_data');
-        $data['list_data'] = MemberAdmin::get_list();
+        $data['list_data'] = $member_admin_obj -> get_list();
 
         return view('admin.admin_list', $data);
     }
@@ -104,11 +109,11 @@ class MemberController extends Controller
      * 新增一筆管理員的資料
      *
      */
-    public function  adminAdd()
+    public function adminAdd()
     {
         $fp = Input::all();
-        MemberAdmin::_init($fp);
-        MemberAdmin::add();
+        $member_admin_obj = new MemberAdminClass($fp);
+        $member_admin_obj -> add();
 
         return '';
     }
@@ -117,11 +122,11 @@ class MemberController extends Controller
      * 更新一筆管理員的資料
      *
      */
-    public function  adminUpdate()
+    public function adminUpdate()
     {
         $fp = Input::all();
-        MemberAdmin::_init($fp);
-        MemberAdmin::update_data();
+        $member_admin_obj = new MemberAdminClass($fp);
+        $member_admin_obj -> update_data();
 
         return '';
     }
@@ -130,13 +135,13 @@ class MemberController extends Controller
      * 移除一個管理員
      *
      */
-    public function  adminDelete()
+    public function adminDelete()
     {
         $fp = Input::all();
-        MemberAdmin::_init($fp);
-        MemberAdmin::delete_data();
+        $member_admin_obj = new MemberAdminClass($fp);
+        $member_admin_obj -> delete_data();
 
-        return ;
+        return;
     }
-	
+
 }
