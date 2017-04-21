5f2fa8f16cdb7efbe21a77cf90966c118e5c6b30
cwc <cwc>
2017-04-22 07:35:30 +0800

    調整超連結

--- resources/views/admin/user/user_add_page.blade.php
+++ resources/views/admin/user/user_add_page.blade.php
@@ -5,9 +5,9 @@
         <div class="record-wrap clearfix">
             <div class="record-menu">
                 <ul>
-                    <li><a href="#" tittle="學校管理">學校管理</a></li>
-                    <li><a class="active" href="#" title="新增使用者">新增使用者</a></li>
-                    <li><a href="#" tittle="匯入使用者">匯入使用者</a></li>
+                    <li><a href="[! route('ad.school.list') !]" tittle="學校管理">學校管理</a></li>
+                    <li><a class="active" href="[! route('ad.user.add.page') !]" title="新增使用者">新增使用者</a></li>
+                    <li><a href="[! route('ad.user.import.page') !]" tittle="匯入使用者">匯入使用者</a></li>
                     <li><a href="#" title="查詢使用者資料">查詢使用者資料</a></li>
                 </ul>
             </div>
