7fecd32f392125f6d7b87088aaad0ae5667bc137
cwc <cwc>
2017-04-21 21:24:21 +0800

    套上新增使用者的路徑

--- resources/views/admin/school_list.blade.php
+++ resources/views/admin/school_list.blade.php
@@ -7,7 +7,7 @@
             <div class="record-menu">
                 <ul>
                     <li><a class="active" href="[! route('ad.school.list') !]" tittle="學校管理">學校管理</a></li>
-                    <li><a href="#" title="新增使用者">新增使用者</a></li>
+                    <li><a href="[! route('ad.user.add.page') !]" title="新增使用者">新增使用者</a></li>
                     <li><a href="#" tittle="匯入使用者">匯入使用者</a></li>
                     <li><a href="#" title="查詢使用者資料">查詢使用者資料</a></li>
                 </ul>
