80872575dd5967a43824ce5a9a57cbf104a2bf79
cwc <cwc>
2017-04-22 23:44:52 +0800

    增加 開放功能 增加 移除開放功能

--- resources/views/admin/subject/subject_limit.blade.php
+++ resources/views/admin/subject/subject_limit.blade.php
@@ -9,45 +9,27 @@
                     <h3 class="search-title">請選取班級及學生</h3>
                     <div class="select-group">
                         <span class="search-title">帳號</span>
-                        <select name="select-account" id="select-account" class="select-s">
+                        <select id="user_id" class="select-s">
                             @foreach($all_teacher as $v)
                                 <option value="[! $v['uid'] !]">[! $v['user_id'] !]【[! $all_level[$v['access_level']] !]】</option>
                             @endforeach
                         </select>
-                        <input id="input-search" class="btn-yellow input-search" type="button" value="選擇完畢，送出">
+                        <input id="input-search" class="btn-yellow input-search" type="button" value="選擇完畢，送出" onclick="chgTableData()">
                     </div>
                 </div>
                 <div class="version-inner clearfix">
                     <div class="unlock-unit-wrap">
                         <h1 class="change-title">無編輯權限之科目</h1>
-                        <select name="unlock-version" id="unlock-version" class="multiple-select" size="12" multiple>
-                            <option value="二階段【數學(單代理人)】">二階段【數學(單代理人)】</option>
-                            <option value="二階段【自然】">二階段【自然】</option>
-                            <option value="二階段【地理】">二階段【地理】</option>
-                            <option value="二階段【理化】">二階段【理化】</option>
-                            <option value="二階段【英文】">二階段【英文】</option>
-                            <option value="二階段【英文】">二階段【英文】</option>
-                            <option value="二階段【生物】">二階段【生物】</option>
-                            <option value="二階段【微積分】">二階段【微積分】</option>
-                            <option value="二階段【音樂】">二階段【音樂】</option>
-                            <option value="二階段【日語】">二階段【日語】</option>
-                            <option value="團指教材【理化】">團指教材【理化】</option>
-                            <option value="團指教材【英文】">團指教材【英文】</option>
+                        <select name="locked-version" id="locked-version" class="multiple-select" size="12" multiple>
                         </select>
                     </div>
                     <div class="lock-button-wrap">
-                        <input type="button" value="開放 →">
-                        <input type="button" value="← 移除">
+                        <input type="button" value="開放 →" onclick="setUnlock()">
+                        <input type="button" value="← 移除" onclick="setLock()">
                     </div>
                     <div class="locked-unit-wrap">
                         <h1 class="change-title">有編輯權限之科目</h1>
-                        <select name="locked-version" id="locked-version" class="multiple-select" size="12" multiple>
-                            <option value="二階段【國語】">二階段【國語】</option>
-                            <option value="團指教材【國語】">團指教材【國語】</option>
-                            <option value="團指教材【數學(單代理人)】">團指教材【數學(單代理人)】</option>
-                            <option value="團指教材【自然】">團指教材【自然】</option>
-                            <option value="團指教材【地理】">團指教材【地理】</option>
-                            <option value="睿耀【數學(單代理人)】">睿耀【數學(單代理人)】</option>
+                        <select name="unlock-version" id="unlock-version" class="multiple-select" size="12" multiple>
                         </select>
                     </div>
                 </div>
@@ -58,4 +40,106 @@
         </div>
     </div>
 </div>
+[! Html::script('js/jquery-1.11.3.js') !]
+<script>
+    var now_user_id = '';
+
+    //更換教師的科目資料
+    function chgTableData()
+    {
+        $("#unlock-version option").remove();
+        $("#locked-version option").remove();
+
+        now_user_id = $('#user_id').val();
+        $.ajax({
+            url: "[! route('ad.subject.lock.unlock') !]",
+            type:'POST',
+            dataType: "json",
+            data: {
+                _token: '[! csrf_token() !]',
+                user_id:$('#user_id').val(),
+            },
+            error: function(xhr) {
+                //alert('Ajax request 發生錯誤');
+            },
+            success: function(response) {
+                var unlock_data = [];
+                //開放的科目資料
+                for( var key in response['access_data'] )
+                {
+
+                    unlock_data.push(response['access_data'][key].toString());
+                }
+                //沒有開放的科目
+                for( var key in response['subject_data'] )
+                {
+                    if(unlock_data.indexOf(key.toString()) != -1)
+                    {
+                        $("#unlock-version").append($("<option></option>").attr("value", key).text(response['subject_data'][key]));
+                    }else{
+                        $("#locked-version").append($("<option></option>").attr("value", key).text(response['subject_data'][key]));
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * 開放科目
+     */
+    function setUnlock()
+    {
+        var unlock = [];
+        $('#locked-version :selected').each(function(i, selected){
+            unlock[i] = $(selected).val();
+        });
+        if(unlock.length > 0)
+        {
+            $.ajax({
+                url: "[! route('ad.subject.set.unlock') !]",
+                type:'POST',
+                data: {
+                    _token: '[! csrf_token() !]',
+                    user_id: now_user_id,
+                    unlock:unlock,
+                },
+                error: function(xhr) {
+                    //alert('Ajax request 發生錯誤');
+                },
+                success: function(response) {
+                    chgTableData();
+                }
+            });
+        }
+    }
+
+    /**
+     * 不開放科目
+     */
+    function setLock()
+    {
+        var lock = [];
+        $('#unlock-version :selected').each(function(i, selected){
+            lock[i] = $(selected).val();
+        });
+        if(lock.length > 0)
+        {
+            $.ajax({
+                url: "[! route('ad.subject.set.lock') !]",
+                type:'POST',
+                data: {
+                    _token: '[! csrf_token() !]',
+                    user_id: now_user_id,
+                    lock:lock,
+                },
+                error: function(xhr) {
+                    //alert('Ajax request 發生錯誤');
+                },
+                success: function(response) {
+                    chgTableData();
+                }
+            });
+        }
+    }
+</script>
 @stop
