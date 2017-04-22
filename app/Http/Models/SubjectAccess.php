423e144376dc06ba1a16c9f0867058e2c58b881d
cwc <cwc>
2017-04-22 22:49:03 +0800

    表單名稱修改為subject_access

---
+++ app/Http/Models/SubjectAccess.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Models;
+
+class SubjectAccess extends BaseModel
+{
+    protected $table = 'subject_access';
+    protected $primaryKey = 'id';
+}
--- app/Http/Models/SubjectLimit.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Http\Models;
-
-class SubjectLimit extends BaseModel
-{
-    protected $table = 'subject_limit';
-    protected $primaryKey = 'id';
-}
