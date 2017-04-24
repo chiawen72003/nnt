17532418af8ea47243e020c1c2ed6a7eab32cfc3
cwc <cwc>
2017-04-24 21:41:55 +0800

    調整 超連結

--- resources/views/admin/exampaper/paper_add.blade.php
+++ resources/views/admin/exampaper/paper_add.blade.php
@@ -6,7 +6,7 @@
         <div class="section-content">
             <ul id="exam-unit-wrap" class="clearfix">
                 <li><a class="current-page" href="[! route('ad.exampaper.add.page') !]">新增試卷</a></li>
-                <li><a href="admin_BuildExam_02.html">新增試題</a></li>
+                <li><a href="[! route('ad.questions.add.page') !]">新增試題</a></li>
                 <li><a href="admin_BuildExam_03.html">編修試卷</a></li>
             </ul>
             <div class="exam-tip">※若以下選單無法選擇，表示您尚未建立任何單元結構！</div>
