6a0038c414743531c95aebe8a54f77158341a889
cwc <cwc>
2017-05-01 21:49:15 +0800

    增加 下載成果紀錄

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,6 +31,9 @@ Route::post('/Mem/Exam/UpdateRecord', ['middleware' => 'MemSessionCheck','as' =>
 Route::get('/Mem/Exam/View/Record/{id}', ['middleware' => 'MemSessionCheck','as' => 'mem.exam.viewrecord', 'uses' => 'ExamController@viewExamRecord']);
 Route::post('/Mem/Exam/semantic/analysis', ['middleware' => 'MemSessionCheck','as' => 'mem.exam.semantic.analysis', 'uses' => 'ExamController@getSemanticAnalysis']);
 
+//下載成果查詢的 excel資料
+Route::get('/Mem/Exam/Download/Record/{id}', ['middleware' => 'MemSessionCheck','as' => 'mem.exam.download.record', 'uses' => 'ExamController@getDownloadRecord']);
+
 //學生端 成果查詢
 Route::get('/Mem/Achievement', ['middleware' => 'MemSessionCheck','as' => 'mem.achievement', 'uses' => 'ExamController@Achievement']);
 Route::get('/Mem/Achievement/List/{id}', ['middleware' => 'MemSessionCheck','as' => 'mem.achievement.list', 'uses' => 'ExamController@AchievementList']);
