f036251e146d5e5e1083fdfd2f0ad0fb9645bf7f
chiawenchen <chia-wen72003>
2017-05-09 14:06:13 +0800

    增加 學習紀錄查詢相關的接口

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -911,4 +911,86 @@ class AdController extends Controller
 
         return ;
     }
+
+    /**
+     * 學習紀錄查詢 首頁
+     */
+    public function ExamRecordListPage()
+    {
+        $fp = Input::all();
+        $data = array();
+        $data['user_data'] = app('request')->session()->get('user_data');
+        $school_tmp = new SchoolClass();
+        $member_tmp = new MemberClass();
+        $member_tmp -> init($fp);
+        $data['city_data'] = $school_tmp -> get_all_city_data();
+        $data['all_school'] = $school_tmp -> get_all_school();
+        $data['all_level'] = $member_tmp -> get_all_level();
+        $data['class_student'] = $member_tmp -> get_class_student_data();
+        $data['city_code'] = isset($fp['city_code'])?$fp['city_code']:null;
+        $data['organization_id'] = isset($fp['organization_id'])?$fp['organization_id']:null;
+        $data['grade'] = isset($fp['grade'])?$fp['grade']:null;
+        $data['class'] = isset($fp['class'])?$fp['class']:null;
+        $data['uid'] = isset($fp['uid'])?$fp['uid']:null;
+        //有指定學生時，取出該學生所有的操作紀錄資料
+        if($data['uid'])
+        {
+            $exam_class_obj = new ExamClass();
+            $subject_obj = new SubjectClass();
+            $data['subject_list'] = $subject_obj -> subject_list();
+            $data['list_data'] = $exam_class_obj -> get_record_list_all_subject($data['uid']);
+        }
+
+
+        return view('admin.examrecord.examrecord_list', $data);
+    }
+
+    /**
+     * 學習紀錄查詢 詳細操作紀錄列表
+     */
+    public function ExamRecordView($id,$uid)
+    {
+        $exam_class_obj = new ExamClass();
+        $data = array();
+        $data['exam_record'] = $exam_class_obj -> get_exam_record($uid,$id);
+        $data['uid'] = $uid;
+
+        return view('ad.examrecord.detail', $data);
+    }
+
+    /**
+     * 學習紀錄查詢 回傳指定班級的所有學生資料
+     */
+    public function ExamRecordStudent()
+    {
+        $fp = Input::all();
+        $return_array = array();
+        $member_tmp = new MemberClass();
+        $member_tmp -> init($fp);
+        $class_student = $member_tmp -> get_all_class_student_data();
+        foreach($class_student as $v)
+        {
+            $return_array[$v['uid']] = $v['name'];
+        }
+
+        return json_encode($return_array);
+    }
+
+    /**
+     * 學習紀錄查詢 下載成果查詢的資料
+     */
+    public function getDownloadRecord($id,$uid)
+    {
+        $exam_class_obj = new ExamClass();
+        if( $uid AND $id)
+        {
+            $exam_record = $exam_class_obj -> get_exam_record($uid,$id);
+            if(isset($exam_record['use_item']) AND count($exam_record['use_item']) > 0)
+            {
+                $excel_obj = new PhpExcel();
+                $excel_obj ->set_excel_data($exam_record['use_item']);
+                $excel_obj ->get_exam_record_file();
+            }
+        }
+    }
 }
