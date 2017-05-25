874012a40f2b829becdef22538e42692fbc82145
cwc <cwc>
2017-05-25 21:32:35 +0800

    調整 更新試題方法

--- app/Http/Providers/QuestionsItemClass.php
+++ app/Http/Providers/QuestionsItemClass.php
@@ -106,12 +106,10 @@ class QuestionsItemClass
         $add_obj->model_item_id = $this->item_data['model_item_id'];
         $add_obj->model_item_options = json_encode($this->item_data['model_item_options']);
         $add_obj->feedback_type = $this->item_data['feedback_type'];
-        $add_obj->exam_paper_id = $this->item_data['exam_paper_id'];
         $add_obj->avatar_type = json_encode($this->item_data['avatar_type']);
         $add_obj->avatar_dsc = json_encode($this->item_data['avatar_dsc']);
         $add_obj->correct_answer = json_encode($this->item_data['correct_answer']);
         $add_obj->error_answer = json_encode($this->item_data['error_answer']);
-        $add_obj->power_dsc = $this->item_data['power_dsc'];
         $add_obj->save();
 
         return $add_obj->id;
@@ -171,13 +169,66 @@ class QuestionsItemClass
     {
         $return_data = array();
 
-        $item_obj = QuestionsItem::where('id', $this->item_data['id'])
+        $item_obj = QuestionsItem::select('questions_item.*','unit_list.module_type')
+            ->leftJoin('exam_paper', 'exam_paper.id', '=', 'questions_item.exam_paper_id')
+            ->leftJoin('unit_list', 'unit_list.id', '=', 'exam_paper.unit_list_id')
+            ->where('questions_item.id', $this->item_data['id'])
             ->get();
         if ($item_obj) {
             foreach ($item_obj as $value) {
                 $return_data = $value->toArray();
             }
+            //部份欄位的json資料要解開
+            if(isset($return_data['avatar_type']))
+            {
+                $return_data['avatar_type'] = json_decode($return_data['avatar_type'],true);
+            }
+            if(isset($return_data['avatar_dsc']))
+            {
+                $t = json_decode($return_data['avatar_dsc'],true);
+                $return_data['avatar_dsc'] = array(
+                    'type' => $t[0]['dsc_type'],
+                    'dsc' => $t[1]['dsc'],
+                );
+            }
+            if(isset($return_data['correct_answer']))
+            {
+                $t = json_decode($return_data['correct_answer'],true);
+                $max_count = count($t[0]['answer']);
+                $return_data['correct_answer'] = array();
+                for($x= 0 ;$x< $max_count;$x++)
+                {
+                    $return_data['correct_answer'][] = array(
+                        'answer' =>$t[0]['answer'][$x],
+                        'jump' =>$t[1]['jump'][$x],
+                        'keyword' =>$t[2]['keyword'][$x],
+                    );
+                }
+            }
+            if(isset($return_data['error_answer']))
+            {
+                $t = json_decode($return_data['error_answer'],true);
+                $max_count = count($t[0]['answer']);
+                $return_data['error_answer'] = array();
+                for($x= 0 ;$x< $max_count;$x++)
+                {
+                    $return_data['error_answer'][] = array(
+                        'answer' =>$t[0]['answer'][$x],
+                        'jump' =>$t[1]['jump'][$x],
+                        'number' =>$t[2]['number'][$x],
+                        'keyword' =>$t[3]['keyword'][$x],
+                    );
+                }
+            }
+            if(isset($return_data['model_item_options']))
+            {
+                $t = json_decode($return_data['model_item_options'],true);
+                $return_data['model_item_options'] = $t;
+            }
+
         }
+
+        //dd($return_data);
 
         return $return_data;
     }
