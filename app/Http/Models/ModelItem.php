14cdc7d5d28ec246d275a0876e53ac5bd81276b4
cwc <cwc>
2017-06-07 21:26:52 +0800

    增加 資料表model_item的model

---
+++ app/Http/Models/ModelItem.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Models;
+
+class ModelItem extends BaseModel
+{
+    protected $table = 'model_item';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+}
