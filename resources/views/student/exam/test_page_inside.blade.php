fe4e18488cf90aad14b035e2cfb8a26ab705fd9e
cwc <cwc>
2017-08-09 21:38:12 +0800

    增加回饋對話的部份

--- resources/views/student/exam/test_page_inside.blade.php
+++ resources/views/student/exam/test_page_inside.blade.php
@@ -8,17 +8,23 @@
     <div id="question-right">
         <iframe width=400 height=125 frameborder=0 scrolling=no
             src="[! $exam_data['iframe_path'] !]"></iframe>
-        <!--
         <div id="qustion-voicetext-wrap">
             <div id="qustion-voicetext-inner">
-                老師回應:近來有越來越多的人喜歡騎腳踏車。騎腳踏車的好處很多，在休閒方面，我們可以騎腳踏車欣賞美麗的風景；在健康方面，我們可以在騎乘的過程中達到鍛鍊身體的運動效果。不妨找個時間，和朋友們一起享受騎腳踏車的樂趣吧！想想看，文章裡提到「騎腳踏車」有哪些好處？寫出一個完整的句子。
-
-
-                老師回應:近來有越來越多的人喜歡騎腳踏車。騎腳踏車的好處很多，在休閒方面，我們可以騎腳踏車欣賞美麗的風景；在健康方面，我們可以在騎乘的過程中達到鍛鍊身體的運動效果。不妨找個時間，和朋友們一起享受騎腳踏車的樂趣吧！想想看，文章裡提到「騎腳踏車」有哪些好處？寫出一個完整的句子。
-                老師回應:近來有越來越多的人喜歡騎腳踏車。騎腳踏車的好處很多，在休閒方面，我們可以騎腳踏車欣賞美麗的風景；在健康方面，我們可以在騎乘的過程中達到鍛鍊身體的運動效果。不妨找個時間，和朋友們一起享受騎腳踏車的樂趣吧！想想看，文章裡提到「騎腳踏車」有哪些好處？寫出一個完整的句子。
+                @if(isset($exam_data['avatar_type']) AND count($exam_data['avatar_type']) == 1)
+                    @if(isset($exam_data['table_dsc']['0']) AND $exam_data['table_dsc']['0'] > '')
+                        老師：[! $exam_data['table_dsc']['0'] !]<br>
+                    @endif
+                @endif
+                @if(isset($exam_data['avatar_type']) AND count($exam_data['avatar_type']) == 2)
+                    @if(isset($exam_data['table_dsc']['0']) AND $exam_data['table_dsc']['0'] > '')
+                        老師：[! $exam_data['table_dsc']['0'] !]<br>
+                    @endif
+                    @if(isset($exam_data['table_dsc']['1']) AND $exam_data['table_dsc']['1'] > '')
+                        學生：[! $exam_data['table_dsc']['1'] !]<br>
+                    @endif
+                @endif
             </div>
         </div>
-        -->
     </div>
 </div>
 [! $exam_item !]
