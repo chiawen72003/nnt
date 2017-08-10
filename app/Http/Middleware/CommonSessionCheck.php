d252ca328b534dafe8462f161293de0759b13efe
cwc <cwc>
2017-08-10 22:26:32 +0800

    增加一個管理員、教師level的session檢查

---
+++ app/Http/Middleware/CommonSessionCheck.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CommonSessionCheck
+{
+    private $com_level = array(
+        21,22,23,31,91,92
+    );
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if ( !$request->session()->has('user_data.access_level') OR
+             !in_array($request->session()->get('user_data.access_level'),$this->com_level)
+            )
+        {
+            return redirect()->route('ad.logout');
+        }
+
+        return $next($request);
+    }
+}
