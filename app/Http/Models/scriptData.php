4a61f641968cda43c69ac119f6cb10b89248c492
cwc <cwc>
2017-11-20 21:50:35 +0800

    增加教學劇本設計資料表

---
+++ app/Http/Models/scriptData.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Models;
+
+class ScriptData extends BaseModel
+{
+    protected $table = 'script_data';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+}
