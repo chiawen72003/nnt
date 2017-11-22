9374dbeec7b14bec098acd74d75c770a6222927c
cwc <cwc>
2017-11-22 21:52:51 +0800

    增加 教學劇本設計路徑 管理員回覆資料表

---
+++ app/Http/Models/scriptAdminData.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Models;
+
+class ScriptAdminData extends BaseModel
+{
+    protected $table = 'script_admin_data';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+}
