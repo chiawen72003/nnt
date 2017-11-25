db94487a7925d971866a9b5cdc8f41d0ad1a2f2b
cwc <cwc>
2017-11-25 23:21:16 +0800

    增加 教師填寫文章數量的列表頁面

--- resources/views/admin/script/script_list.blade.php
+++ resources/views/admin/script/script_list.blade.php
@@ -22,266 +22,267 @@
                             <td></td>
                             <td>閱讀文本</td>
                             <td>[! (isset($script_data['obj_1']))?$script_data['obj_1']:'0'!]</td>
-                            <td>obj_1</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_1')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>主要問題</td>
                             <td>[! (isset($script_data['obj_2']))?$script_data['obj_2']:'0'!]</td>
-                            <td>obj_2</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_2')) !]"></a></td>
+                            <td></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>學生作答1</td>
                             <td>[! (isset($script_data['obj_3']))?$script_data['obj_3']:'0'!]</td>
-                            <td>obj_3</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_3')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>被誘導錯答</td>
                             <td>[! (isset($script_data['obj_4']))?$script_data['obj_4']:'0'!]</td>
-                            <td>obj_4</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_4')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>不好的答案</td>
                             <td>[! (isset($script_data['obj_5']))?$script_data['obj_5']:'0'!]</td>
-                            <td>obj_5</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_5')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>其他答案</td>
                             <td>[! (isset($script_data['obj_6']))?$script_data['obj_6']:'0'!]</td>
-                            <td>obj_6</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_6')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>空白</td>
                             <td>[! (isset($script_data['obj_7']))?$script_data['obj_7']:'0'!]</td>
-                            <td>obj_7</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_7')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>教師正向回饋</td>
                             <td>[! (isset($script_data['obj_8']))?$script_data['obj_8']:'0'!]</td>
-                            <td>obj_8</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_8')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>後設認知的回饋</td>
                             <td>[! (isset($script_data['obj_9']))?$script_data['obj_9']:'0'!]</td>
-                            <td>obj_9</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_9')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>教師中性回饋</td>
                             <td>[! (isset($script_data['obj_10']))?$script_data['obj_10']:'0'!]</td>
-                            <td>obj_10</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_10')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>教師激發反應</td>
                             <td>[! (isset($script_data['obj_11']))?$script_data['obj_11']:'0'!]</td>
-                            <td>obj_11</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_11')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>教師總結摘要</td>
                             <td>[! (isset($script_data['obj_12']))?$script_data['obj_12']:'0'!]</td>
-                            <td>obj_12</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_12')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>符合預期答案</td>
                             <td>[! (isset($script_data['obj_13']))?$script_data['obj_13']:'0'!]</td>
-                            <td>obj_13</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_13')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>總結對話</td>
                             <td>[! (isset($script_data['obj_14']))?$script_data['obj_14']:'0'!]</td>
-                            <td>obj_14</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_14')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>提供線索</td>
                             <td>[! (isset($script_data['obj_15']))?$script_data['obj_15']:'0'!]</td>
-                            <td>obj_15</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_15')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>對話結束</td>
                             <td>[! (isset($script_data['obj_16']))?$script_data['obj_16']:'0'!]</td>
-                            <td>obj_16</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_16')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>學生回答2</td>
                             <td>[! (isset($script_data['obj_17']))?$script_data['obj_17']:'0'!]</td>
-                            <td>obj_17</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_17')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>不好的答案</td>
                             <td>[! (isset($script_data['obj_18']))?$script_data['obj_18']:'0'!]</td>
-                            <td>obj_18</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_18')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>其他答案</td>
                             <td>[! (isset($script_data['obj_19']))?$script_data['obj_19']:'0'!]</td>
-                            <td>obj_19</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_19')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>正確答案</td>
                             <td>[! (isset($script_data['obj_20']))?$script_data['obj_20']:'0'!]</td>
-                            <td>obj_20</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_20')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>教師正向回饋</td>
                             <td>[! (isset($script_data['obj_21']))?$script_data['obj_21']:'0'!]</td>
-                            <td>obj_21</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_21')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>教師中性回饋</td>
                             <td>[! (isset($script_data['obj_22']))?$script_data['obj_22']:'0'!]</td>
-                            <td>obj_22</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_22')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>教師激發反應</td>
                             <td>[! (isset($script_data['obj_23']))?$script_data['obj_23']:'0'!]</td>
-                            <td>obj_23</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_23')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>教師明確提示</td>
                             <td>[! (isset($script_data['obj_24']))?$script_data['obj_24']:'0'!]</td>
-                            <td>obj_24</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_24')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>學生回答3</td>
                             <td>[! (isset($script_data['obj_25']))?$script_data['obj_25']:'0'!]</td>
-                            <td>obj_25</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_25')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>教師判定</td>
                             <td>[! (isset($script_data['obj_26']))?$script_data['obj_26']:'0'!]</td>
-                            <td>obj_26</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_26')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>教師提供答案</td>
                             <td>[! (isset($script_data['obj_27']))?$script_data['obj_27']:'0'!]</td>
-                            <td>obj_27</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_27')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>教師正向回饋</td>
                             <td>[! (isset($script_data['obj_28']))?$script_data['obj_28']:'0'!]</td>
-                            <td>obj_28</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_28')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>教師正向回饋</td>
                             <td>[! (isset($script_data['obj_29']))?$script_data['obj_29']:'0'!]</td>
-                            <td>obj_29</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_29')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>正確答案</td>
                             <td>[! (isset($script_data['obj_30']))?$script_data['obj_30']:'0'!]</td>
-                            <td>obj_30</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_30')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>錯誤答案</td>
                             <td>[! (isset($script_data['obj_31']))?$script_data['obj_31']:'0'!]</td>
-                            <td>obj_31</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_31')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>正確答案</td>
                             <td>[! (isset($script_data['obj_32']))?$script_data['obj_32']:'0'!]</td>
-                            <td>obj_32</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_32')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>教師正向回饋</td>
                             <td>[! (isset($script_data['obj_33']))?$script_data['obj_33']:'0'!]</td>
-                            <td>obj_33</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_33')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>教師提問</td>
                             <td>[! (isset($script_data['obj_34']))?$script_data['obj_34']:'0'!]</td>
-                            <td>obj_34</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_34')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>學生回答2</td>
                             <td>[! (isset($script_data['obj_35']))?$script_data['obj_35']:'0'!]</td>
-                            <td>obj_35</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_35')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>教師正向回饋</td>
                             <td>[! (isset($script_data['obj_36']))?$script_data['obj_36']:'0'!]</td>
-                            <td>obj_36</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_36')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>教師總結</td>
                             <td>[! (isset($script_data['obj_37']))?$script_data['obj_37']:'0'!]</td>
-                            <td>obj_37</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_37')) !]"></a></td>
                         </tr>
                         <tr>
                             <td></td>
                             <td></td>
                             <td>對話結束</td>
                             <td>[! (isset($script_data['obj_38']))?$script_data['obj_38']:'0'!]</td>
-                            <td>obj_38</td>
+                            <td><a class="icon-action icon-edit" href="[! route('ad.script.review', array('uid'=>$uid,'item_key'=>'obj_38')) !]"></a></td>
                         </tr>
                     </table>
                 </form>
