8307372481b54eaa3f4bb688040ee0b15aebb8ba
cwc <cwc>
2017-12-08 22:22:05 +0800

    增加序號資料

--- resources/views/admin/script/prompt_list.blade.php
+++ resources/views/admin/script/prompt_list.blade.php
@@ -17,266 +17,266 @@
                             <th class="th-button">功能</th>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>1</td>
                             <td></td>
                             <td>閱讀文本</td>
                             <td>[! (isset($list_data['obj_1']))?$list_data['obj_1']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_1')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>2</td>
                             <td></td>
                             <td>主要問題</td>
                             <td>[! (isset($list_data['obj_2']))?$list_data['obj_2']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_2')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>3</td>
                             <td></td>
                             <td>學生作答1</td>
                             <td>[! (isset($list_data['obj_3']))?$list_data['obj_3']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_3')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>4</td>
                             <td></td>
                             <td>被誘導錯答</td>
                             <td>[! (isset($list_data['obj_4']))?$list_data['obj_4']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_4')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>5</td>
                             <td></td>
                             <td>不好的答案</td>
                             <td>[! (isset($list_data['obj_5']))?$list_data['obj_5']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_5')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>6</td>
                             <td></td>
                             <td>其他答案</td>
                             <td>[! (isset($list_data['obj_6']))?$list_data['obj_6']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_6')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>7</td>
                             <td></td>
                             <td>空白</td>
                             <td>[! (isset($list_data['obj_7']))?$list_data['obj_7']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_7')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>8</td>
                             <td></td>
                             <td>教師正向回饋</td>
                             <td>[! (isset($list_data['obj_8']))?$list_data['obj_8']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_8')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>9</td>
                             <td></td>
                             <td>後設認知的回饋</td>
                             <td>[! (isset($list_data['obj_9']))?$list_data['obj_9']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_9')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>10</td>
                             <td></td>
                             <td>教師中性回饋</td>
                             <td>[! (isset($list_data['obj_10']))?$list_data['obj_10']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_10')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>11</td>
                             <td></td>
                             <td>教師激發反應</td>
                             <td>[! (isset($list_data['obj_11']))?$list_data['obj_11']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_11')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>12</td>
                             <td></td>
                             <td>教師總結摘要</td>
                             <td>[! (isset($list_data['obj_12']))?$list_data['obj_12']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_12')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>13</td>
                             <td></td>
                             <td>符合預期答案</td>
                             <td>[! (isset($list_data['obj_13']))?$list_data['obj_13']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_13')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>14</td>
                             <td></td>
                             <td>總結對話</td>
                             <td>[! (isset($list_data['obj_14']))?$list_data['obj_14']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_14')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>15</td>
                             <td></td>
                             <td>提供線索</td>
                             <td>[! (isset($list_data['obj_15']))?$list_data['obj_15']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_15')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>16</td>
                             <td></td>
                             <td>對話結束</td>
                             <td>[! (isset($list_data['obj_16']))?$list_data['obj_16']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_16')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>17</td>
                             <td></td>
                             <td>學生回答2</td>
                             <td>[! (isset($list_data['obj_17']))?$list_data['obj_17']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_17')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>18</td>
                             <td></td>
                             <td>不好的答案</td>
                             <td>[! (isset($list_data['obj_18']))?$list_data['obj_18']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_18')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>19</td>
                             <td></td>
                             <td>其他答案</td>
                             <td>[! (isset($list_data['obj_19']))?$list_data['obj_19']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_19')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>20</td>
                             <td></td>
                             <td>正確答案</td>
                             <td>[! (isset($list_data['obj_20']))?$list_data['obj_20']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_20')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>21</td>
                             <td></td>
                             <td>教師正向回饋</td>
                             <td>[! (isset($list_data['obj_21']))?$list_data['obj_21']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_21')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>22</td>
                             <td></td>
                             <td>教師中性回饋</td>
                             <td>[! (isset($list_data['obj_22']))?$list_data['obj_22']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_22')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>23</td>
                             <td></td>
                             <td>教師激發反應</td>
                             <td>[! (isset($list_data['obj_23']))?$list_data['obj_23']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_23')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>24</td>
                             <td></td>
                             <td>教師明確提示</td>
                             <td>[! (isset($list_data['obj_24']))?$list_data['obj_24']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_24')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>25</td>
                             <td></td>
                             <td>學生回答3</td>
                             <td>[! (isset($list_data['obj_25']))?$list_data['obj_25']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_25')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>26</td>
                             <td></td>
                             <td>教師判定</td>
                             <td>[! (isset($list_data['obj_26']))?$list_data['obj_26']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_26')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>27</td>
                             <td></td>
                             <td>教師提供答案</td>
                             <td>[! (isset($list_data['obj_27']))?$list_data['obj_27']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_27')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>28</td>
                             <td></td>
                             <td>教師正向回饋</td>
                             <td>[! (isset($list_data['obj_28']))?$list_data['obj_28']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_28')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>29</td>
                             <td></td>
                             <td>教師正向回饋</td>
                             <td>[! (isset($list_data['obj_29']))?$list_data['obj_29']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_29')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>30</td>
                             <td></td>
                             <td>正確答案</td>
                             <td>[! (isset($list_data['obj_30']))?$list_data['obj_30']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_30')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>31</td>
                             <td></td>
                             <td>錯誤答案</td>
                             <td>[! (isset($list_data['obj_31']))?$list_data['obj_31']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_31')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>32</td>
                             <td></td>
                             <td>正確答案</td>
                             <td>[! (isset($list_data['obj_32']))?$list_data['obj_32']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_32')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>33</td>
                             <td></td>
                             <td>教師正向回饋</td>
                             <td>[! (isset($list_data['obj_33']))?$list_data['obj_33']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_33')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>34</td>
                             <td></td>
                             <td>教師提問</td>
                             <td>[! (isset($list_data['obj_34']))?$list_data['obj_34']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_34')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>35</td>
                             <td></td>
                             <td>學生回答2</td>
                             <td>[! (isset($list_data['obj_35']))?$list_data['obj_35']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_35')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>36</td>
                             <td></td>
                             <td>教師正向回饋</td>
                             <td>[! (isset($list_data['obj_36']))?$list_data['obj_36']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_36')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>37</td>
                             <td></td>
                             <td>教師總結</td>
                             <td>[! (isset($list_data['obj_37']))?$list_data['obj_37']:''!]</td>
                             <td><a class="icon-action icon-edit" href="[! route('ad.script.prompt.edit', array('item_key'=>'obj_37')) !]"></a></td>
                         </tr>
                         <tr>
-                            <td></td>
+                            <td>38</td>
                             <td></td>
                             <td>對話結束</td>
                             <td>[! (isset($list_data['obj_38']))?$list_data['obj_38']:''!]</td>
